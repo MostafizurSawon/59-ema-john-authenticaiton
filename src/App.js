7808360772ffafd1d035128c58fa7259672812e3
MostafizurSawon <MostafizurSawon@users.noreply.github.com>
2021-10-19 20:54:49 +0600

    assignment git

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import NotFound from './components/NotFound/NotFound';
 import OrderReview from './components/OrderReview/OrderReview';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import Shop from './components/Shop/Shop';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import AuthProvider from './context/AuthProvider';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Shipping from './components/Shipping/Shipping';
 
 function App() {
   return (
     <div>
+      <AuthProvider>
       <Router>
         <Header></Header>
         <Switch>
@@ -22,17 +28,27 @@ function App() {
           <Route path="/review">
             <OrderReview></OrderReview>
           </Route>
-          <Route path="/inventory">
+          <PrivateRoute path="/inventory">
             <Inventory></Inventory>
-          </Route>
-          <Route path="/placeorder">
+          </PrivateRoute>
+          <PrivateRoute path="/shipping">
+            <Shipping></Shipping>
+          </PrivateRoute>
+          <PrivateRoute path="/placeorder">
             <PlaceOrder></PlaceOrder>
+          </PrivateRoute>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
           </Route>
           <Route path="*">
             <NotFound></NotFound>
           </Route>
         </Switch>
       </Router>
+      </AuthProvider>
 
     </div>
   );
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhU-D5vUVOsjteWuDTcectGMJRGFiVs5k",
+    authDomain: "ema-john-authentication-b9bc9.firebaseapp.com",
+    projectId: "ema-john-authentication-b9bc9",
+    storageBucket: "ema-john-authentication-b9bc9.appspot.com",
+    messagingSenderId: "359771014821",
+    appId: "1:359771014821:web:23a5bb9fec1a55e0e7af4c"
+  };
+
+  export default firebaseConfig;
